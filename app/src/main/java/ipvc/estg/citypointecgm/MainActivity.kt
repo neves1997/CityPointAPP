56fbd43fbfb8fe3a47a1b3b4de5b57e19f6a53eb
neves1997 <deandrefilipe@gmail.com>
2021-03-16 22:37:24 +0000

    Creat Project
    Criação do projecto de Computação móvel 2020/21

---
+++ app/src/main/java/ipvc/estg/citypointecgm/MainActivity.kt
@@ -0,0 +1,39 @@
+package ipvc.estg.citypointecgm
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import ipvc.estg.citypointecgm.MainActivity as MainActivity1
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //classe
+    }
+
+
+    //evento butão login
+    //fazer validações
+
+    fun buttonlogin(view: View) {
+
+
+
+    }
+
+    fun buttonnotas(view: View) {
+
+        val intent = Intent (this, nota::class.java).apply {
+        }
+
+        startActivity(intent)
+
+    }
+
+
+}
---
+++ app/src/main/java/ipvc/estg/citypointecgm/nota.kt
@@ -0,0 +1,18 @@
+package ipvc.estg.citypointecgm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class nota : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nota)
+    }
+
+    fun insert(view: View) {
+
+
+
+    }
+}
